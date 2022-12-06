ab644ac42dabc4970f77692033d77ffd04e7b264
BT_4N2_13 <hsun7920@bths.edu>
2022-12-06 11:51:13 -0500

    Initial commit

---
+++ src/Time.java
@@ -0,0 +1,11 @@
+public class Time {
+    private int h;
+    private int m;
+    private int s;
+
+    public Time(int hours, int mins, int secs){
+        h = hours;
+        m = mins;
+        s = secs;
+    }
+}
